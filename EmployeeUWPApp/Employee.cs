26f27f64fb5480c800d9f9859d317e5aee12eb30
Kal <info@kalacademy.org>
2019-05-19 12:23:42 -0700

    Add project files.

---
+++ EmployeeUWPApp/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeUWPApp
+{
+    class Employee
+    {
+        private const string TEXT_FILE_NAME = "Employee.txt";
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public async static Task<ICollection<Employee>> GetEmployeesAsync()
+        {
+            var employees = new List<Employee>();
+            var content = await FileHelper.ReadTextFileAsync(TEXT_FILE_NAME);
+            var lines = content.Split('\r', '\n');
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                var lineParts = line.Split(',');
+                var employee = new Employee
+                {
+                    Name = lineParts[0],
+                    Title = lineParts[1]
+                };
+                employees.Add(employee);
+            }
+
+            return employees;
+
+        }
+
+        public static void WriteEmployee(Employee employee)
+        {
+            var employeeData = $"{employee.Name}, {employee.Title}";
+            FileHelper.WriteTextFileAsync(TEXT_FILE_NAME, employeeData);
+        }
+    }
+}
---
+++ EmployeeUWPApp/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace EmployeeUWPApp
+{
+    static class FileHelper
+    {
+        public async static void WriteTextFileAsync(
+            string filename, string content)
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+        }
+
+        public static async Task<string> ReadTextFileAsync
+            (string filename)
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await storageFolder.GetFileAsync(filename);
+            var textStream = await textFile.OpenReadAsync();
+            var textReader = new DataReader(textStream);
+            var textLength = textStream.Size;
+            await textReader.LoadAsync((uint)textLength);
+            return textReader.ReadString((uint)textLength);
+        }
+    }
+}
